f519b0a77e6e04a1c8bed4bd3b6b3e9595a7d798
cchamom <cchamom2@miumg.edu.gt>
2025-07-30 14:19:05 -0600

    Cuarta version

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ print("Hola Mundo")
 a = int(input("Escribe un número: "))
 b = int(input("Escribe otro numero: "))
 c = a + b
+print("===================================")
 print("La suma de los números es:", c)
 
 print("===================================")
 c = a - b
 print("La resta de los números es:", c)
+print("===================================")
+
+c = a * b
+print("La multiplicación de los números es:", c)
+print("===================================")
