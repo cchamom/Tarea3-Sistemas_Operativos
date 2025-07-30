a90d048eba8df29cf21bf9ebb4f608e247c21996
cchamom <cchamom2@miumg.edu.gt>
2025-07-30 14:21:26 -0600

    Quinta version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hola Mundo")
+print("Tarea 3-Sistemas Operativos")
+print("Versiones Dde Git y GitHub")
+print("===================================")
 a = int(input("Escribe un número: "))
 b = int(input("Escribe otro numero: "))
 c = a + b
@@ -13,3 +15,5 @@ print("===================================")
 c = a * b
 print("La multiplicación de los números es:", c)
 print("===================================")
+c = a / b
+print("La división de los números es:", c)
